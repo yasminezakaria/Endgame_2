a92042cdecd5d1627fcfb520fed016d3e0809c65
yasminezakaria <yasminezakaria@outlook.com>
2019-11-18 23:03:11 +0200

    Implemented ingrid predicate

--- Endgame.pl
+++ Endgame.pl
@@ -3,32 +3,33 @@ s(1,1,s0).
 s(2,1,s0).
 s(2,2,s0).
 s(3,3,s0).
-t(3,4,s0).
+t(3,4).
 m(5).
 n(5).
-action(down, 1, 0).
-action(up, -1, 0).
-
 
 action(right, 0, 1).
 action(left, 0, -1).
+action(down, 1, 0).
+action(up, -1, 0).
 
-
+ingrid(X,Y):- m(I), X < I, X>=0, n(J), Y < J, Y>=0.
 valid_move(X,Y,A):-
     action(A,DX, DY), NX is X + DX, NY is Y + DY, m(I), NX < I, NX>=0, n(J), NY < J, NY>=0.
 im(X,Y,result(A,S)):-
     % valid_move(X,Y,a),action(a, DX, DY), NX is X + DX, NY is Y + DY, im(NX, NY, S).
     % valid_move(X,Y,A),
-    valid_move(X,Y,A),action(A, DX, DY), NX is X - DX, NY is Y - DY, im(NX, NY, S).
+    ingrid(X,Y), action(A, DX, DY), NX is X - DX, NY is Y - DY, im(NX, NY, S).
 % , call_with_depth_limit(snapped(S), 100, R)
 snapped(S):-
-    im(X,Y,T),t(X,Y,s0).
+    t(X,Y), im(X,Y,S).
 
-/*iterative_deepening(snapped(S), L, R):-
-call_with_depth_limit(snapped(S), L, R), \+ (R='depth_limit_exceeded').
 
 iterative_deepening(snapped(S), L, 'depth_limit_exceeded'):-
-    N is L+1, call_with_depth_limit(snapped(S), N, R).*/
+    N is L+1, call_with_depth_limit(snapped(S), N, R).
+iterative_deepening(snapped(S), L, R):-
+    call_with_depth_limit(snapped(S), L, R), \+(R=='depth_limit_exceeded').
+    % N is L+1, iterative_deepening(snapped(S), N, R).
+
 
 % The argument R of the call_with_depth limit predicate is bound to depth_limit_exceeded 
 % when no solution is found within the specified depth. 
